7c22e056383fa4f5d752048027669365d35e29ac
fabiana.zabala <fabianazabala@hotmail.es>
2020-07-03 20:53:31 +0200

    Here I have created the workshops. Janusz, MarianAuto and Adrian

---
+++ src/main/java/shop/cars/CarParts.java
@@ -0,0 +1,9 @@
+package shop.cars;
+
+public abstract class CarParts {
+  public String brakes;
+  public String dampers;
+  public String engine;
+  public String carBody;
+  public String gearbox;
+}
